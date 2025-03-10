be46409955c02b38c93dffa68781dfc59ed1d3c9
Jaimin Rana <jaiminrana1102@gmail.com>
2025-03-10 16:39:28 +0530

    validation added at database level

---
+++ Backend/config/setup.js
@@ -0,0 +1,128 @@
+import { connectDB, client } from "./Dbconnection.js";
+
+const db = await connectDB();
+
+const status = "create";
+
+try {
+    // schema validation users
+    await db.command({
+        [status]: "users",
+        validator: {
+            $jsonSchema: {
+                required: [
+                    '_id',
+                    'name',
+                    'email',
+                    'password',
+                    'rootdir'
+                ],
+                properties: {
+                    _id: {
+                        bsonType: 'objectId'
+                    },
+                    name: {
+                        bsonType: 'string',
+                        maxLength: 45
+                    },
+                    email: {
+                        bsonType: 'string',
+                        pattern: '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$'
+                    },
+                    password: {
+                        bsonType: 'string',
+                        minLength: 4,
+                        maxLength: 8
+                    },
+                    rootdir: {
+                        bsonType: 'objectId'
+                    }
+                },
+                additionalProperties: false
+            }
+        },
+        validationLevel: 'strict',
+        validationAction: "error"
+    });
+
+    // schema validation directories
+    await db.command({
+        [status]: "directories",
+        validator: {
+            $jsonSchema: {
+                required: [
+                    '_id',
+                    'name',
+                    'parent',
+                    'files',
+                    'directories',
+                    'userId'
+                ],
+                properties: {
+                    _id: {
+                        bsonType: 'objectId'
+                    },
+                    name: {
+                        bsonType: 'string'
+                    },
+                    parent: {
+                        bsonType: [
+                            'objectId',
+                            'null'
+                        ]
+                    },
+                    files: {
+                        bsonType: 'array'
+                    },
+                    directories: {
+                        bsonType: 'array'
+                    },
+                    userId: {
+                        bsonType: 'objectId'
+                    }
+                },
+                additionalProperties: false
+            }
+        },
+        validationLevel: 'strict',
+        validationAction: "error"
+    });
+
+    // schema validation files
+    await db.command({
+        [status]: "files",
+        validator: {
+            $jsonSchema: {
+                required: [
+                    '_id',
+                    'ext',
+                    'name',
+                    'dirId'
+                ],
+                properties: {
+                    _id: {
+                        bsonType: 'objectId'
+                    },
+                    ext: {
+                        bsonType: 'string'
+                    },
+                    name: {
+                        bsonType: 'string'
+                    },
+                    dirId: {
+                        bsonType: 'objectId'
+                    }
+                },
+                additionalProperties: false
+            }
+        },
+        validationLevel: 'strict',
+        validationAction: "error"
+    });
+
+} catch (error) {
+    console.log("Error while setting up database!", error);
+} finally {
+    await client.close();
+    process.exit(0);
+}
